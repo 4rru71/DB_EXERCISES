70c0db0f92aa3a32f69df0f046c6c207abd031d6
Arruti <arruti750@gmail.com>
2025-04-28 19:55:36 -0600

    Ejercicios realizados hoy

---
+++ customer/query.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT 
+    customer_id,
+    COUNT(*) AS count_no_trans 
+FROM 
+    visits 
+    LEFT JOIN transactions USING(visit_id) 
+WHERE 
+    transaction_id IS NULL 
+GROUP BY 
+    customer_id;
---
+++ rising_temperature/query.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+
+SELECT t1.id 
+FROM weather t1
+JOIN weather t2
+    ON DATEDIFF(t1.recordDate, t2.recordDate) = 1
+WHERE t1.temperature > t2.temperature;
